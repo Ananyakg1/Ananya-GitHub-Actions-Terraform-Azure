8aa58cd9f04942cc13ef17e7a9af7b779c370063
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-22 22:56:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the Resource Group"
-  default     = "TerraformDemo"
+  default     = "AnanyaResourceGroup"
 }
 
 variable "location" {
@@ -10,7 +10,7 @@ variable "location" {
 
 variable "storage_account_name" {
   description = "The name of the Storage Account (must be 3-24 characters, lowercase, letters, and numbers only)"
-  default     = "terraformcodean"
+  default     = "AnanyaDemoStorage"
 
   validation {
     condition     = length(var.storage_account_name) >= 3 && length(var.storage_account_name) <= 24 && var.storage_account_name == lower(var.storage_account_name)
@@ -20,7 +20,7 @@ variable "storage_account_name" {
 
 variable "container_name" {
   description = "The name of the Blob Container"
-  default     = "tfstatefile"
+  default     = "AnanyatfStateFile"
 }
 
 variable "tfstate_key" {
