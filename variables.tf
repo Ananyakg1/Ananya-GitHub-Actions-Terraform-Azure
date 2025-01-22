43722f21f898688e40d8c8f5232c8ddce6a006ca
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-22 14:30:20 +0530

    Added terraform files

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "The name of the Resource Group"
+  default     = "TerraformDemo"
+}
+
+variable "location" {
+  description = "The location for resources"
+  default     = "East US"
+}
+
+variable "storage_account_name" {
+  description = "The name of the Storage Account (must be 3-24 characters, lowercase, letters, and numbers only)"
+  default     = "terraformcodean"
+
+  validation {
+    condition     = length(var.storage_account_name) >= 3 && length(var.storage_account_name) <= 24 && var.storage_account_name == lower(var.storage_account_name)
+    error_message = "Storage account name must be 3-24 characters long, lowercase, and contain only letters and numbers."
+  }
+}
+
+variable "container_name" {
+  description = "The name of the Blob Container"
+  default     = "tfstatefile"
+}
+
+variable "tfstate_key" {
+  description = "The name of the Terraform state file"
+  default     = "dev.terraform.tfstate"
+}
