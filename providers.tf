df485198c6eb7472a12bcdf4b3b7023d4fb72c46
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-22 15:46:53 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -9,11 +9,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  subscription_id = env.AZURE_SUBSCRIPTION_ID
-  client_id       = env.AZURE_AD_CLIENT_ID
-  client_secret   = env.AZURE_AD_CLIENT_SECRET
-  tenant_id       = env.AZURE_AD_TENANT_ID
 }
  
 
