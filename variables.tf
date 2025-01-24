9a1659bcee5ac83edebcf351b8bee7b61f75fc3d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-24 14:44:34 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the Resource Group"
-  default     = "ananyaresourcegroupnew"
+  default     = "ananyaresourcegroupnew1"
 }
 
 variable "location" {
@@ -10,7 +10,7 @@ variable "location" {
 
 variable "storage_account_name" {
   description = "The name of the Storage Account (must be 3-24 characters, lowercase, letters, and numbers only)"
-  default     = "ananya1demostoragenew"
+  default     = "ananya1demostoragenew1"
 
   validation {
     condition     = length(var.storage_account_name) >= 3 && length(var.storage_account_name) <= 24 && var.storage_account_name == lower(var.storage_account_name)
@@ -20,7 +20,7 @@ variable "storage_account_name" {
 
 variable "container_name" {
   description = "The name of the Blob Container"
-  default     = "ananya1tfstatefilenew"
+  default     = "ananya1tfstatefilenew1"
 }
 
 variable "tfstate_key" {
