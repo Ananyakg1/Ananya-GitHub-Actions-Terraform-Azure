f65b4c230446697a628ad34d043e518703d9afc3
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-24 12:50:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the Resource Group"
-  default     = "TerraformDemo"
+  default     = "ananyaresourcegroup"
 }
 
 variable "location" {
