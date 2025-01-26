5b9d9ae6697c3b90722038add01487d216dfdeef
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-26 12:56:54 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -10,11 +10,15 @@ terraform {
 provider "azurerm" {
 alias = "other"
   features {
-    
   }
-  subscription_id = "AZURE_SUBSCRIPTION_ID"
+}
+
+provider "azurerm" {
+  alias           = "other"
+  features {}
+
   tenant_id       = "AZURE_AD_TENANT_ID"
   client_id       = "AZURE_AD_CLIENT_ID"
   client_secret   = "AZURE_AD_CLIENT_SECRET"
- 
+  subscription_id = "AZURE_SUBSCRIPTION_ID"
 }
