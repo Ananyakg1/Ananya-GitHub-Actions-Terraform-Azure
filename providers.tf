768df6926f4bb34717a83f6511baeec442d36fbf
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-27 10:03:39 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -12,12 +12,4 @@ provider "azurerm" {
   }
 }
 
-provider "azurerm" {
-  alias           = "other"
-  features {}
 
-  tenant_id       = "AZURE_AD_TENANT_ID"
-  client_id       = "AZURE_AD_CLIENT_ID"
-  client_secret   = "AZURE_AD_CLIENT_SECRET"
-  subscription_id = "AZURE_SUBSCRIPTION_ID"
-}
