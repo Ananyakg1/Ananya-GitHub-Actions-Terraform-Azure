04783400639ccf5beb47f4542dad420324ad5439
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 10:11:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,62 @@
+# Define environment variable (dev, qa, prod)
+variable "environment" {
+  description = "The environment (e.g., dev, qa, prod)"
+  type        = string
+  default     = "dev" # Default value, can be overridden per environment
+}
+
+# Resource group names for different environments
 variable "resource_group_name_dev" {
-  description = "The name of the Resource Group for Dev"
-  default     = "resourcegroup-dev-unique"  # New name for Dev resource group
+  description = "The name of the Resource Group for the dev environment"
+  default     = "resourcegroup-dev"
 }
 
 variable "resource_group_name_qa" {
-  description = "The name of the Resource Group for QA"
-  default     = "resourcegroup-qa-unique"   # New name for QA resource group
+  description = "The name of the Resource Group for the QA environment"
+  default     = "resourcegroup-qa"
 }
 
 variable "resource_group_name_prod" {
-  description = "The name of the Resource Group for Prod"
-  default     = "resourcegroup-prod-unique" # New name for Prod resource group
+  description = "The name of the Resource Group for the prod environment"
+  default     = "resourcegroup-prod"
 }
 
-variable "location" {
-  description = "The location for resources"
-  default     = "East US"
-}
-
+# Storage account names for different environments
 variable "storage_account_name_dev" {
-  description = "The name of the Storage Account for Dev"
-  default     = "storagedevunique"  # New name for Dev storage account
+  description = "The name of the Storage Account for the dev environment"
+  default     = "storagedev"
 }
 
 variable "storage_account_name_qa" {
-  description = "The name of the Storage Account for QA"
-  default     = "storageqaunique"   # New name for QA storage account
+  description = "The name of the Storage Account for the qa environment"
+  default     = "storageqa"
 }
 
 variable "storage_account_name_prod" {
-  description = "The name of the Storage Account for Prod"
-  default     = "storageprodunique" # New name for Prod storage account
+  description = "The name of the Storage Account for the prod environment"
+  default     = "storageprod"
 }
 
+# Blob container names for different environments
 variable "container_name_dev" {
-  description = "The name of the Blob Container for Dev"
-  default     = "containerdevunique"  # New name for Dev container
+  description = "The name of the container for the dev environment"
+  default     = "container-dev"
 }
 
 variable "container_name_qa" {
-  description = "The name of the Blob Container for QA"
-  default     = "containerqaunique"   # New name for QA container
+  description = "The name of the container for the qa environment"
+  default     = "container-qa"
 }
 
 variable "container_name_prod" {
-  description = "The name of the Blob Container for Prod"
-  default     = "containerprodunique" # New name for Prod container
+  description = "The name of the container for the prod environment"
+  default     = "container-prod"
+}
+
+# Location of resources
+variable "location" {
+  description = "The location of the resources"
+  default     = "East US"
 }
 
 
