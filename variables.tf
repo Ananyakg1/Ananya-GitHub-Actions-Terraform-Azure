df87d0bde0ac35ed710829ded601802e8065c88d
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 09:50:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,49 +11,49 @@ variable "location" {
 # Unique Resource Group Names
 variable "resource_group_name_dev" {
   description = "Resource Group name for dev environment"
-  default     = "resourcegroup-dev"
+  default     = "resourcegroup1-dev"
 }
 
 variable "resource_group_name_qa" {
   description = "Resource Group name for qa environment"
-  default     = "resourcegroup-qa"
+  default     = "resourcegroup1-qa"
 }
 
 variable "resource_group_name_prod" {
   description = "Resource Group name for prod environment"
-  default     = "resourcegroup-prod"
+  default     = "resourcegroup1-prod"
 }
 
 # Unique Storage Account Names (Must be globally unique)
 variable "storage_account_name_dev" {
   description = "Storage Account name for dev environment"
-  default     = "storagedevnew123"
+  default     = "1storagedevnew123"
 }
 
 variable "storage_account_name_qa" {
   description = "Storage Account name for qa environment"
-  default     = "storageqanew123"
+  default     = "1storageqanew123"
 }
 
 variable "storage_account_name_prod" {
   description = "Storage Account name for prod environment"
-  default     = "storageprodnew123"
+  default     = "1storageprodnew123"
 }
 
 # Unique Container Names
 variable "container_name_dev" {
   description = "Blob Container name for dev environment"
-  default     = "tfstatefile-dev"
+  default     = "tfstatefile1-dev"
 }
 
 variable "container_name_qa" {
   description = "Blob Container name for qa environment"
-  default     = "tfstatefile-qa"
+  default     = "tfstatefile1-qa"
 }
 
 variable "container_name_prod" {
   description = "Blob Container name for prod environment"
-  default     = "tfstatefile-prod"
+  default     = "tfstatefile1-prod"
 }
 
 variable "tfstate_key" {
