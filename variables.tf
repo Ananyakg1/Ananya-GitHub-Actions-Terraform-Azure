dbc21a6bee2b135e494c355abe2a2fdeee6f2351
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 10:15:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,63 +1,43 @@
-# Define environment variable (dev, qa, prod)
 variable "environment" {
-  description = "The environment (e.g., dev, qa, prod)"
+  description = "The environment (dev, qa, prod)"
   type        = string
-  default     = "dev" # Default value, can be overridden per environment
 }
 
-# Resource group names for different environments
-variable "resource_group_name_dev" {
-  description = "The name of the Resource Group for the dev environment"
-  default     = "resourcegroup-dev"
-}
-
-variable "resource_group_name_qa" {
-  description = "The name of the Resource Group for the QA environment"
-  default     = "resourcegroup-qa"
-}
-
-variable "resource_group_name_prod" {
-  description = "The name of the Resource Group for the prod environment"
-  default     = "resourcegroup-prod"
-}
-
-# Storage account names for different environments
-variable "storage_account_name_dev" {
-  description = "The name of the Storage Account for the dev environment"
-  default     = "storagedev"
-}
-
-variable "storage_account_name_qa" {
-  description = "The name of the Storage Account for the qa environment"
-  default     = "storageqa"
-}
-
-variable "storage_account_name_prod" {
-  description = "The name of the Storage Account for the prod environment"
-  default     = "storageprod"
-}
-
-# Blob container names for different environments
-variable "container_name_dev" {
-  description = "The name of the container for the dev environment"
-  default     = "container-dev"
-}
-
-variable "container_name_qa" {
-  description = "The name of the container for the qa environment"
-  default     = "container-qa"
-}
-
-variable "container_name_prod" {
-  description = "The name of the container for the prod environment"
-  default     = "container-prod"
-}
-
-# Location of resources
 variable "location" {
-  description = "The location of the resources"
+  description = "The location for resources"
   default     = "East US"
 }
+
+variable "resource_group_names" {
+  description = "Resource group names for different environments"
+  type        = map(string)
+  default = {
+    dev  = "resourcegroup-dev"
+    qa   = "resourcegroup-qa"
+    prod = "resourcegroup-prod"
+  }
+}
+
+variable "storage_account_names" {
+  description = "Storage account names for different environments"
+  type        = map(string)
+  default = {
+    dev  = "storagedev123"
+    qa   = "storageqa123"
+    prod = "storageprod123"
+  }
+}
+
+variable "container_names" {
+  description = "Blob container names for different environments"
+  type        = map(string)
+  default = {
+    dev  = "tfstatefile-dev"
+    qa   = "tfstatefile-qa"
+    prod = "tfstatefile-prod"
+  }
+}
+
 
 
 
