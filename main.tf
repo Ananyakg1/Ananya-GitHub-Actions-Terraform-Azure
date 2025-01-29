e851528136af8333790a729d61da3e71ea04f68f
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 10:14:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
 resource "azurerm_resource_group" "rg" {
-  name     = var.environment == "dev" ? var.resource_group_name_dev : 
-             var.environment == "qa" ? var.resource_group_name_qa : var.resource_group_name_prod
+  name     = var.resource_group_names[var.environment]
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.environment == "dev" ? var.storage_account_name_dev : 
-                            var.environment == "qa" ? var.storage_account_name_qa : var.storage_account_name_prod
-  resource_group_name       = azurerm_resource_group.rg.name
+  name                     = var.storage_account_names[var.environment]
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
-  account_tier              = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "container" {
-  name                    = var.environment == "dev" ? var.container_name_dev : 
-                            var.environment == "qa" ? var.container_name_qa : var.container_name_prod
-  storage_account_name    = azurerm_storage_account.storage.name
-  container_access_type   = "private"
+  name                  = var.container_names[var.environment]
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "private"
 }
 
 
