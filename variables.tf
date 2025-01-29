0d3e36b335366fd65e34f8071646a9449db9de5a
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 11:18:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "environment" {
   description = "The environment (dev, qa, prod)"
-  type        = string
+  default     = "dev"
 }
 
 variable "location" {
@@ -8,35 +8,42 @@ variable "location" {
   default     = "East US"
 }
 
-variable "resource_group_names" {
-  description = "Resource group names for different environments"
-  type        = map(string)
-  default = {
-    dev  = "resourcegroup-dev"
-    qa   = "resourcegroup-qa"
-    prod = "resourcegroup-prod"
-  }
+variable "resource_group_name_dev" {
+  default = "resourcegroup-dev"
 }
 
-variable "storage_account_names" {
-  description = "Storage account names for different environments"
-  type        = map(string)
-  default = {
-    dev  = "storagedev123"
-    qa   = "storageqa123"
-    prod = "storageprod123"
-  }
+variable "resource_group_name_qa" {
+  default = "resourcegroup-qa"
 }
 
-variable "container_names" {
-  description = "Blob container names for different environments"
-  type        = map(string)
-  default = {
-    dev  = "tfstatefile-dev"
-    qa   = "tfstatefile-qa"
-    prod = "tfstatefile-prod"  
-  }
+variable "resource_group_name_prod" {
+  default = "resourcegroup-prod"
 }
+
+variable "storage_account_name_dev" {
+  default = "storagedevnew123"
+}
+
+variable "storage_account_name_qa" {
+  default = "storageqanew123"
+}
+
+variable "storage_account_name_prod" {
+  default = "storageprodnew123"
+}
+
+variable "container_name_dev" {
+  default = "tfstatefile-dev"
+}
+
+variable "container_name_qa" {
+  default = "tfstatefile-qa"
+}
+
+variable "container_name_prod" {
+  default = "tfstatefile-prod"
+}
+
 
 
 
