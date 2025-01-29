f88c44a3db9de333ef5ca979c085afa78b9df189
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 12:52:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -9,39 +9,39 @@ variable "location" {
 }
 
 variable "resource_group_name_dev" {
-  default = "newresourcegroup1-dev"
+  default = "mainresourcegroup1-dev"
 }
 
 variable "resource_group_name_qa" {
-  default = "new1resourcegroup1-qa"
+  default = "main1resourcegroup1-qa"
 }
 
 variable "resource_group_name_prod" {
-  default = "new2resourcegroup1-prod"
+  default = "main2resourcegroup1-prod"
 }
 
 variable "storage_account_name_dev" {
-  default = "newstoragedevnew123"
+  default = "mainstoragedevnew123"
 }
 
 variable "storage_account_name_qa" {
-  default = "new1storageqanew123"
+  default = "main1storageqanew123"
 }
 
 variable "storage_account_name_prod" {
-  default = "new2storageprodnew123"
+  default = "main2storageprodnew123"
 }
 
 variable "container_name_dev" {
-  default = "newtfstatefile-dev"
+  default = "maintfstatefile-dev"
 }
 
 variable "container_name_qa" {
-  default = "new1tfstatefile-qa"
+  default = "main1tfstatefile-qa"
 }
 
 variable "container_name_prod" {
-  default = "new2tfstatefile-prod"
+  default = "main2tfstatefile-prod"
 }
 
 
