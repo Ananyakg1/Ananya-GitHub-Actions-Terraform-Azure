0fab025a2fe773ddfa969cc9edfc88b2a1928229
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 09:22:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.resource_group_name}-${var.environment}" # Appends environment suffix to the base name
+  name     = "${var.resource_group_name}-${var.environment}" # Unique name per environment
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "${var.storage_account_name}${var.environment}" # Appends environment suffix to the base name
+  name                     = "${var.storage_account_name}${var.environment}" # Ensuring uniqueness
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
@@ -12,8 +12,10 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = "${var.container_name}-${var.environment}" # Appends environment suffix to the base name
+  name                  = "${var.container_name}-${var.environment}" # Appends environment suffix
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
+
+
 
