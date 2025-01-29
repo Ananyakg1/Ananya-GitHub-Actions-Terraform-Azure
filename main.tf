5d3a65641e63851a002069b7807dc71589fd6527
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-29 09:56:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
 resource "azurerm_resource_group" "rg" {
-  name     = lookup({
-    dev  = var.resource_group_name_dev,
-    qa   = var.resource_group_name_qa,
-    prod = var.resource_group_name_prod
-  }, var.environment, var.resource_group_name_dev)  # Default to dev if env is missing
+  name     = var.environment == "dev" ? var.resource_group_name_dev : 
+             var.environment == "qa" ? var.resource_group_name_qa : 
+             var.resource_group_name_prod
   location = var.location
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = lookup({
-    dev  = var.storage_account_name_dev,
-    qa   = var.storage_account_name_qa,
-    prod = var.storage_account_name_prod
-  }, var.environment, var.storage_account_name_dev)  # Default to dev
-
+  name                     = var.environment == "dev" ? var.storage_account_name_dev : 
+                             var.environment == "qa" ? var.storage_account_name_qa : 
+                             var.storage_account_name_prod
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
@@ -21,12 +16,9 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "container" {
-  name                  = lookup({
-    dev  = var.container_name_dev,
-    qa   = var.container_name_qa,
-    prod = var.container_name_prod
-  }, var.environment, var.container_name_dev)  # Default to dev
-
+  name                  = var.environment == "dev" ? var.container_name_dev : 
+                          var.environment == "qa" ? var.container_name_qa : 
+                          var.container_name_prod
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
