3ec14312426e0e3bbed806f72dc8054272e6720b
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 10:18:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,11 @@
 output "resource_group_name" {
-  description = "The name of the Resource Group"
-  value       = azurerm_resource_group.rg.name
+  value = azurerm_resource_group.rg.name
 }
 
 output "storage_account_name" {
-  description = "The name of the Storage Account"
-  value       = azurerm_storage_account.storage.name
+  value = azurerm_storage_account.sa.name
 }
 
 output "container_name" {
-  description = "The name of the Blob Container"
-  value       = azurerm_storage_container.container.name
+  value = azurerm_storage_container.sc.name
 }
