026d7142f68ae79e9db87e27f18edd6606295a3c
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 10:14:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,51 +1,17 @@
-variable "env" {
-  description = "The environment name"
-  type        = string
-}
-variable "environment" {
-  description = "The environment (dev, qa, prod)"
-  default     = "dev"
-}
-
 variable "location" {
-  description = "The location for resources"
+  description = "Azure region for deployment"
+  type        = string
   default     = "East US"
 }
 
-variable "resource_group_name_dev" {
-  default = "demoresourcegroup1-dev"
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
 }
 
-variable "resource_group_name_qa" {
-  default = "demoresourcegroup1-qa"
-}
-
-variable "resource_group_name_prod" {
-  default = "demoresourcegroup1-prod"
-}
-
-variable "storage_account_name_dev" {
-  default = "demostoragedevnew123"
-}
-
-variable "storage_account_name_qa" {
-  default = "demostorageqanew123"
-}
-
-variable "storage_account_name_prod" {
-  default = "demostorageprodnew123"
-}
-
-variable "container_name_dev" {
-  default = "demotfstatefile-dev"
-}
-
-variable "container_name_qa" {
-  default = "demotfstatefile-qa"
-}
-
-variable "container_name_prod" {
-  default = "demotfstatefile-prod"
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
 }
 
 
