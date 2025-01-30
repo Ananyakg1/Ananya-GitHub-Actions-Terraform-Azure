ac6aaebc3a36756ec6fff8db24f364f99dea129b
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-01-30 10:14:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,31 @@
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group - Uses a variable for dynamic environment selection
 resource "azurerm_resource_group" "rg" {
-  name     = "demores-${var.env}"
+  name     = var.resource_group_name
   location = var.location
 }
 
+# Storage Account - Example resource
 resource "azurerm_storage_account" "storage" {
-  name                     = "demosto-${var.env}"
+  name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = var.location
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = "democon-${var.env}"
-  storage_account_name  = azurerm_storage_account.storage.name
-  container_access_type = "private"
+# Output values
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
 
 
 
